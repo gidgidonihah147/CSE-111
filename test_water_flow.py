17f90f0f04883012d32dc1f4259ab4b8253794a1
gidgidonihah147 <gidgidonihah@byui.edu>
2024-10-26 15:51:33 -0600

    06 Prove Assignment: Troubleshooting Functions
    Second set of water_flow.py and test_water_flow.py. Finished tests and added Stretch work.

--- test_water_flow.py
+++ test_water_flow.py
@@ -1,12 +1,12 @@
 #-----------------WEEK 05 ASSIGNMENT---------------#
 
-#-----------------Step 2---------------#
+#-----------------Week 05 - Step 2---------------#
 # Create another new file, save it as test_water_flow.py, and copy and paste the following import statements into the file.
 from pytest import approx
 import pytest
 
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
-#-----------------Step 4---------------#
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynold_numbers, pressure_loss_from_pipe_reduction
+#-----------------Week 05 - Step 4---------------#
 # In your test_water_flow.py file, write a test function named test_water_column_height. This test function must call water_column_height at least four times to verify that it is working correctly. Use the following numbers in your test function.
 # Tower Height | Tank Wall Height |Expected Water Column Height
 # 0	   | 0    |	0
@@ -21,7 +21,7 @@ def test_water_column_height():
     assert water_column_height(48.3, 12.8) == approx(57.9, rel=0.01)
 
 
-#-----------------Step 6---------------#
+#-----------------Week 05 - Step 6---------------#
 # In your test_water_flow.py file, write a test function named test_pressure_gain_from_water_height. This test function must call pressure_gain_from_water_height at least three times to verify that it is working correctly. Use the following numbers in your test function.
 # Height | Expected Pressure | Aprox absolute Tolerance
 # 0	   |  0       |	0.001
@@ -34,7 +34,7 @@ def test_pressure_gain_from_water_height():
     assert pressure_gain_from_water_height(50.0) == approx(489.450, rel=0.001)
 
 
-#-----------------Step 8---------------#
+#-----------------Week 05 - Step 8---------------#
 # In your test_water_flow.py file, write a test function named test_pressure_loss_from_pipe . This test function must call pressure_loss_from_pipe at least seven times to verify that it is working correctly. Use the following numbers in your test function.
 # Pipe Diameter | Pipe Length | Friction Factor | Fluid Velocity | Expected Pressure Loss | Aprox absolute Tolerance
 # 0.048692 | 0	     | 0.018 | 1.75 | 0	       | 0.001
@@ -54,9 +54,57 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, rel=0.001)
     assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, rel=0.001)
 
-#-----------------Step 9---------------#
+#-----------------Week 05 - Step 9---------------#
 # Copy and paste the following code at the bottom of your test_water_flow.py file.
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
 pytest.main(["-v", "--tb=line", "-rN", __file__])
+
+
+#-----------------Week 06 - Step 2---------------#
+# In your test_water_flow.py file, write a test function named test_pressure_loss_from_fittings. This test function must call pressure_loss_from_fittings at least five times to verify that it is working correctly. Use the following numbers in your test function.
+# Fluid Velocity | Quantity of Fittings | Expected Pressure Loss | approx Absolute Tolerance
+# 0    |	3 |	0      |	0.001
+# 1.65 |	0 |	0      |	0.001
+# 1.65 |	2 |	-0.109 |	0.001
+# 1.75 |	2 |	-0.122 |	0.001
+# 1.75 |	5 |	-0.306 |	0.001
+
+def test_pressure_loss_from_fittings():
+    assert pressure_loss_from_fittings(0.00, 3) == approx(0.000, rel=0.001)
+    assert pressure_loss_from_fittings(1.65, 0) == approx(0.000, rel=0.001)
+    assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, rel=0.01)
+    assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, rel=0.01)
+    assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, rel=0.001)
+
+#-----------------Week 06 - Step 4---------------#
+# In your test_water_flow.py file, write a test function named test_reynolds_number. This test function must call reynolds_number at least five times to verify that it is working correctly. Use the following numbers in your test function.
+# Hydraulic Diameter | Fluid Velocity | Expected Reynolds Number | approx Absolute Tolerance
+# 0.048692 | 0    | 0      | 1
+# 0.048692 | 1.65 | 80069  | 1
+# 0.048692 | 1.75 | 84922  | 1
+# 0.28687  | 1.65 | 471729 | 1
+# 0.28687  | 1.75 | 500318 | 1
+
+def test_reynold_number():
+    assert reynold_numbers(0.048692, 0.00) == approx(0, rel=1)
+    assert reynold_numbers(0.048692, 1.65) == approx(80069, rel=1)
+    assert reynold_numbers(0.048692, 1.75) == approx(84922, rel=1)
+    assert reynold_numbers(0.286870, 1.65) == approx(471729, rel=1)
+    assert reynold_numbers(0.286870, 1.75) == approx(500318, rel=1)
+
+
+#-----------------Week 06 - Step 6---------------#
+# In your test_water_flow.py file, write a test function named test_pressure_loss_from_pipe_reduction. This test function must call pressure_loss_from_pipe_reduction at least three times to verify that it is working correctly. Use the following numbers in your test function.
+# Larger Diameter | Fluid Velocity | Reynolds Number | Smaller Diameter | Expected Pressure Loss | approx Absolute Tolerance
+# 0.28687 | 0    | 1      | 0.048692 | 0        | 0.001
+# 0.28687 | 1.65 | 471729 | 0.048692 | -163.744 | 0.001
+# 0.28687 | 1.75 | 500318 | 0.048692 | -184.182 | 0.001
+
+def test_pressure_loss_from_pipe_reduction():
+    assert pressure_loss_from_pipe_reduction(0.28687, 0.00, 1, 0.048692) == approx(0.000, rel=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, rel=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, rel=0.001)
+
+
--- water_flow.py
+++ water_flow.py
@@ -15,10 +15,18 @@
 #         == approx(-0.306, abs=0.001)
 # Notice in the previous example, that the value for abs is 0.001, which causes the approx function to ignore all digits after the third digit after the decimal point.
 
-#-----------------Step 1---------------#
+#-----------------WEEK 06 ASSIGNMENT---------------#
+# Purpose
+# Prove that you can write a Python program and write and run test functions to help you find and fix mistakes in your program.
+
+# Assignment
+# Write the second half of the Python program to help an engineer design a water distribution system that you began in the previous lesson’s prove milestone. Also, write more test functions that will automatically verify that your program functions work correctly.
+
+
+#-----------------Week 05 - Step 1---------------#
 # Using VS Code, create a new file and save it as water_flow.py
 
-#-----------------Step 3---------------#
+#-----------------Week 05 - Step 3---------------#
 # In your water_flow.py program, write a function named water_column_height that calculates and returns the height of a column of water from a tower height and a tank wall height. The function must have this header:
 # def water_column_height(tower_height, tank_height):
 # In your function, use the following formula for calculating the water column height.
@@ -31,7 +39,7 @@
 def water_column_height(tower_height, tank_height):
     return tower_height + 3 * tank_height / 4
 
-#-----------------Step 5---------------#
+#-----------------Week 05 - Step 5---------------#
 # In your water_flow.py program, write a function named pressure_gain_from_water_height that calculates and returns the pressure caused by Earth’s gravity pulling on the water stored in an elevated tank. The function must have this header:
 # def pressure_gain_from_water_height(height):
 # In your function, use the following formula for calculating pressure caused by Earth’s gravity.
@@ -48,7 +56,7 @@ def pressure_gain_from_water_height(height):
     g = 9.80665
     return rho * g * height / 1000
 
-#-----------------Step 7---------------#
+#-----------------Week 05 - Step 7---------------#
 # In your water_flow.py program, write a function named pressure_loss_from_pipe that calculates and returns the water pressure lost because of the friction between the water and the walls of a pipe that it flows through. The function must have this header:
 # def pressure_loss_from_pipe(pipe_diameter,
 #         pipe_length, friction_factor, fluid_velocity):
@@ -66,3 +74,121 @@ def pressure_gain_from_water_height(height):
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
     rho = 998.2
     return -friction_factor * pipe_length * rho * fluid_velocity**2 / (2000 * pipe_diameter)
+
+#-----------------Week 06 - Step 1---------------#
+# In your water_flow.py program, write a function named pressure_loss_from_fittings that calculates the water pressure lost because of fittings such as 45° and 90° bends that are in a pipeline. The function must have this header:
+# def pressure_loss_from_fittings(
+#         fluid_velocity, quantity_fittings):
+# In your function, use the following formula for calculating pressure loss from pipe fittings.
+
+# P = (−0.04ρv2n)/2000 
+# where
+# P is the lost pressure in kilopascals
+# ρ is the density of water (998.2 kilogram / meter3)
+# v is the velocity of the water flowing through the pipe in meters / second
+# n is the quantity of fittings
+
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    rho = 998.2
+    return -0.04 * rho * fluid_velocity**2 * quantity_fittings / 2000
+
+#-----------------Week 06 - Step 3---------------#
+# In your water_flow.py program write a function named reynolds_number that calculates and returns the Reynolds number for a pipe with water flowing through it. The Reynolds number is a unitless ratio of the inertial and viscous forces in a fluid that is useful for predicting fluid flow in different situations. The function must have this header.
+# def reynolds_number(hydraulic_diameter, fluid_velocity):
+# In your function, use the following formula for calculating the Reynolds number.
+
+# R = ρdv/μ
+# where
+# R is the Reynolds number
+# ρ is the density of water (998.2 kilogram / meter3)
+# d is the hydraulic diameter of a pipe in meters. For a round pipe, the hydraulic diameter is the same as the pipe’s inner diameter.
+# v is the velocity of the water flowing through the pipe in meters / second
+# μ is the dynamic viscosity of water (0.0010016 Pascal seconds)
+
+def reynold_numbers(hydraulic_diameter, fluid_velocity):
+    rho = 998.2
+    dynamic_viscosity = 0.0010016
+    return rho * hydraulic_diameter * fluid_velocity / dynamic_viscosity
+
+
+#-----------------Week 06 - Step 5---------------#
+# In your water_flow.py program write a function named pressure_loss_from_pipe_reduction that calculates the water pressure lost because of water moving from a pipe with a large diameter into a pipe with a smaller diameter. The function must have this header.
+# def pressure_loss_from_pipe_reduction(larger_diameter,
+#         fluid_velocity, reynolds_number, smaller_diameter):
+# In your function, use the following two formulas for calculating pressure loss from a rounded reduction in a pipe’s diameter.
+
+# k = (0.1 + 50) ((D/d)^4-1)
+# P = (−kρv^2)/2000 
+# where
+# k is a constant computed by the first formula and used in the second formula
+# R is the Reynolds number that corresponds to the pipe with the larger diameter
+# D is the diameter of the larger pipe in meters
+# d is the diameter of the smaller pipe in meters
+# P is the lost pressure kilopascals
+# ρ is the density of water (998.2 kilogram / meter3)
+# v is the velocity of the water flowing through the larger diameter pipe in meters / second
+
+def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynold_number, smaller_diameter):
+    rho = 998.2
+    k = (0.1 + 50/reynold_number) * ((larger_diameter/smaller_diameter)**4 - 1)
+    return -k * rho * fluid_velocity**2 / 2000
+
+#-----------------Week 06 - Stretch 2---------------#
+# The functions that you wrote for this assignment, calculate water pressure in kilopascals (kPa). In the United States, water pressure is usually expressed in pounds per square inch (psi). Write a function in your water_flow.py program that converts kPa to psi. Then at the bottom of your main function, add code that calls your conversion function and prints the final pressure value in both kPa and psi.
+
+def kpa_to_psi(pressure_kpa):
+  pressure_psi = pressure_kpa * 0.145038
+  return pressure_psi
+
+#-----------------Week 06 - Step 7---------------#
+# Copy and paste the following code at the bottom of your water_flow.py program.
+PVC_SCHED80_INNER_DIAMETER = 0.28687 # (meters)  11.294 inches
+PVC_SCHED80_FRICTION_FACTOR = 0.013  # (unitless)
+SUPPLY_VELOCITY = 1.65               # (meters / second)
+
+HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
+HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
+HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
+
+
+def main():
+    tower_height = float(input("Height of water tower (meters): "))
+    tank_height = float(input("Height of water tank walls (meters): "))
+    length1 = float(input("Length of supply pipe from tank to lot (meters): "))
+    quantity_angles = int(input("Number of 90° angles in supply pipe: "))
+    length2 = float(input("Length of pipe from supply to house (meters): "))
+
+    water_height = water_column_height(tower_height, tank_height)
+    pressure = pressure_gain_from_water_height(water_height)
+
+    diameter = PVC_SCHED80_INNER_DIAMETER
+    friction = PVC_SCHED80_FRICTION_FACTOR
+    velocity = SUPPLY_VELOCITY
+    reynolds = reynold_numbers(diameter, velocity)
+    loss = pressure_loss_from_pipe(diameter, length1, friction, velocity)
+    pressure += loss
+
+    loss = pressure_loss_from_fittings(velocity, quantity_angles)
+    pressure += loss
+
+    loss = pressure_loss_from_pipe_reduction(diameter,
+            velocity, reynolds, HDPE_SDR11_INNER_DIAMETER)
+    pressure += loss
+
+    diameter = HDPE_SDR11_INNER_DIAMETER
+    friction = HDPE_SDR11_FRICTION_FACTOR
+    velocity = HOUSEHOLD_VELOCITY
+    loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
+    pressure += loss
+
+    print(f"Pressure at house: {pressure:.1f} kilopascals")
+
+#-----------------Week 06 - Stretch 2---------------#
+    # Convert kPa to psi and print the result
+    pressure_psi = kpa_to_psi(pressure)
+    print(f"Pressure at house: {pressure_psi:.2f} psi")
+
+
+#-----------------Week 06 - Step 7---------------#
+if __name__ == "__main__":
+    main()
